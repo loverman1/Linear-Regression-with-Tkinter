f52765501425f8b04a1e60d8eee4501da6a1047e
SuperNova <52492864+AchrafBella@users.noreply.github.com>
2020-12-08 16:52:22 +0100

    Add files via upload

--- src/GUI.py
+++ src/GUI.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Dec  7 10:04:46 2020
 
-@author: frsiabel
+@author: supernova
 """
 from tkinter import *
 import tkinter as tk
@@ -11,6 +11,7 @@ import pandas as pd
 
 class GUI(tk.Tk):
     def __init__(self):
+        #parameters
         tk.Tk.__init__(self)
         self.geometry('1000x600')
         self.title('Visualization of your prediction')
@@ -18,42 +19,57 @@ class GUI(tk.Tk):
         self.resizable(width = False, height = False)
         self.path = None
         
-        self.f1 = tk.Frame(self, width = 1000, height = 100, bg = "DeepSkyblue4",
+        #frames
+        self.f1 = tk.Frame(self, bd=10, width = 1000, height = 100, bg = "DeepSkyblue4",
                  relief = SUNKEN)
         self.f1.pack(side = TOP)
+        self.f2 = tk.Frame(self, bd=5, width = 500, height = 500, bg = "cyan",
+                 relief = RIDGE)
+        self.f2.pack(side = RIGHT)
+        self.f3 = tk.Frame(self, bd=5, width = 500, height = 500, bg = "cyan",
+                 relief = RIDGE)
+        self.f3.pack(side = LEFT)
         
-        self.f2 = tk.Frame(self, width = 1000, height = 500, bg = "cyan",
-                 relief = SUNKEN)
-        self.f2.pack(side = TOP)
+        Label(self.f1, font=('arial', 30, 'bold'), bg = "DeepSkyblue4",
+              text='Visualization of your prediction', bd = 7).pack()            
+        self.my_algo = ['LinearRegression', 'LogisticRegression', 'DecisionTree',
+                  'SVM', 'NaiveBayes', 'kNN', 'K-Means', 'RandomForest']
+        self.listbox_algo = Listbox(self.f3)
+        self.listbox_algo.grid(row=1 , column=0)
         
-        lb1 = tk.Label(self.f2, font=('arial',10,'bold'), text="upload the file:"
-                      ,bg="cyan",fg="black")
-        lb1.grid(padx=50,pady=20, row= 0,column=0)
-         
-        bt1 = tk.Button(self.f2,padx=50,pady=10,bg="cyan", text=" clicked ",
-                        command=lambda :self.upload())
-        bt1.grid(padx=50,pady=20,row=0 , column=1)
-         
-        bt2 = tk.Button(self.f2,padx=50,pady=10,bg="cyan", text=" clicked ",
-                        command=lambda :self.features())
-        bt2.grid(padx=50,pady=20,row=1 , column=0)  
+        for i, elm in enumerate(self.my_algo):
+            self.listbox_algo.insert(i, str(elm))
+            pass
         
-        self.listbox_features = Listbox(self.f2)
-        self.listbox_features.grid(padx = 50, pady = 20, row=1 , column=1)
+        self.listbox_features = Listbox(self.f3)
+        self.listbox_features.grid(row=1 , column=1)
         
-        bt3 = tk.Button(self.f2,padx=50,pady=10,bg="cyan", text="choose algrithm ",
+        bt1 = tk.Button(self.f3,bg="cyan", text="select the algorithem",
                         command=lambda :self.Algorithme())
-        bt3.grid(padx=50,pady=20,row=2 , column=0)  
+        bt1.grid(row=0 , column=0)
         
-        self.listbox_algo = Listbox(self.f2)
-        self.listbox_algo.grid(padx = 50, pady = 20, row=2 , column=1)
-    
+        bt2 = tk.Button(self.f3,bg="cyan", text="upload data",
+                        command=lambda :self.upload())
+        bt2.grid(row=0 , column=1)
+        
+        bt3 = tk.Button(self.f3,bg="cyan", text="choose your features",
+                        command=lambda :self.features())
+        bt3.grid(row=0 , column=3)
+        
+        bt3 = tk.Button(self.f3,bg="cyan", text="choose your features",
+                        command=lambda :self.features())
+        bt3.grid(row=0 , column=3) 
+        
+        
+        
+        
+        
+    # functions 
     def upload(self):
         self.path = filedialog.askopenfilename(initialdir = "/",
                                                    title = "Select file",
-                      filetypes = (("jpeg files","*.csv"),("all files","*.*")))
-        print(self.path)
-    
+                filetypes = (("jpeg files","*.csv"),("all files","*.*")))
+ 
     def features(self):
         columns = None
         try:
@@ -65,13 +81,29 @@ class GUI(tk.Tk):
         for i, elm in enumerate(columns):
             self.listbox_features.insert(i, elm)
             pass
- 
+  
+    def Algorithem(self):
+        try:
+            t = self.listbox_algo.curselection()
+            algo = self.my_algo[t[0]]
+            print(algo)
+        except:
+            print('selection an algorithm')
+            
+    def selection_features(self):
+        try:
+            t = self.listbox_algo.curselection()
+            algo = self.my_algo[t[0]]
+            print(algo)
+        except:
+            print('selection a feature')
         
-    def Algorithme(self):
-        l = "LinearRegression LogisticRegression DecisionTree SVM NaiveBayes kNN K-Means RandomForest"
-        for i, elm in enumerate(l.split(' ')):
-            self.listbox_algo.insert(i, str(elm))
-        pass
+        
+        
+        
+        
+        
+        
 
 if __name__ == '__main__':
     gui = GUI().mainloop()
--- src/logInGUI.py
+++ src/logInGUI.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Dec  7 10:25:31 2020
 
-@author: frsiabel
+@author: supernova
 """
 from tkinter import *
 import tkinter as tk
@@ -10,25 +10,43 @@ from GUI import GUI
 
 class LogIn_GUI(tk.Tk):
     def __init__(self):
+        #parameters
         tk.Tk.__init__(self)
-        
         self.geometry('1000x600')
         self.title('Visualization of your prediction')
         self.configure(background='cyan')
-        self.resizable(width=False,height=False)
-    
+        self.resizable(width = False, height = False)
         
-        Label(self,font=('arial',20,'bold'), text="Username" ,
+        #frames
+        self.f1 = tk.Frame(self, bd=10, width = 1000, height = 100, bg = "DeepSkyblue4",
+                 relief = SUNKEN)
+        self.f1.pack(side = TOP)
+        
+        self.f2 = tk.Frame(self, bd=5, width = 500, height = 500, bg = "cyan",
+                 relief = RIDGE)
+        self.f2.pack(side = TOP)
+        
+        self.f3 = tk.Frame(self.f2, bd=5, width = 500, height = 500, bg = "cyan",
+                 relief = RIDGE)
+        self.f3.pack(side = TOP)
+        
+        Label(self.f1, font=('arial', 30, 'bold'), bg = "DeepSkyblue4",
+              text='Visualization of your prediction', bd = 7).pack()
+        
+        #buttons and Entries
+        Label(self.f3,font=('arial',20,'bold'), text="Username" ,
                          bg="cyan",fg="black").pack(pady = 5) 
-        self.username = Entry(self)
+        
+        self.username = Entry(self.f3)
         self.username.pack(pady = 5)
         
-        Label(self,font=('arial',20,'bold'), text="Password" ,
+        Label(self.f3,font=('arial',20,'bold'), text="Password" ,
                         bg="cyan",fg="black").pack(pady = 5) 
-        self.password = Entry(self, show='*')
+        
+        self.password = Entry(self.f3, show = '*')
         self.password.pack(pady = 5)
 
-        tk.Button(self, padx = 5 ,pady = 5 ,bg="cyan",
+        tk.Button(self.f3, padx = 5 ,pady = 5 ,bg="cyan",
                               text = "connect ", 
                               command = lambda : self.long_in()).pack(pady = 5)
     def long_in(self):
